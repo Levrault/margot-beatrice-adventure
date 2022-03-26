bfc5fb809deddbd118229e36c0182a01cc75199c
levrault <lucf.langis@gmail.com>
2022-03-26 10:04:29 -0400

    Update icon atlas texture

--- src/Autoload/InputManager.gd
+++ src/Autoload/InputManager.gd
@@ -165,16 +165,36 @@ func get_device_button_from_action(action: String, for_device: String) -> String
 	return result
 
 
-func get_device_icon_texture_from_action(input: String, for_device: String) -> AtlasTexture:
+func get_device_icon_texture_from_action(input: String, for_device: String, alt := false) -> AtlasTexture:
 	if for_device == InputManager.DEVICE_MOUSE:
 		for_device = InputManager.DEVICE_KEYBOARD
 	if for_device == InputManager.DEVICE_KEYBOARD:
 		input = "KEY_" + input.to_upper()
 
+	if alt:
+		for_device += "_alt"
+
+	if not atlas_map.has(for_device) or not atlas_map[for_device].has(input):
+		return null
+
 	var texture = AtlasTexture.new()
 	texture.atlas = load(ICON_ATLAS_TEXTURE_PATH)
 	var atlas_region = atlas_map[for_device][input]
 	texture.region = Rect2(atlas_region[0], atlas_region[1], atlas_region[2], atlas_region[3])
+
+	return texture
+
+
+func get_device_icon_texture_fallback(for_device: String) -> AtlasTexture:
+	if for_device == InputManager.DEVICE_MOUSE:
+		for_device = InputManager.DEVICE_KEYBOARD
+
+	var texture = AtlasTexture.new()
+	texture.atlas = load(ICON_ATLAS_TEXTURE_PATH)
+
+	var atlas_region = atlas_map["fallback"]["TEMPLATE"]
+	texture.region = Rect2(atlas_region[0], atlas_region[1], atlas_region[2], atlas_region[3])
+
 	return texture
 
 
---
+++ src/UI/Commons/TextureRect/AtlasIcon.gd
@@ -0,0 +1,56 @@
+extends TextureRect
+
+enum Icon { DEFAULT, ALT }
+
+const DURATION := 1.0
+
+export var action := ""
+
+var icon_texture :AtlasTexture = null
+var alt_icon_texture : AtlasTexture = null
+var next_icon :int = Icon.ALT
+
+onready var anim := $AnimationPlayer
+onready var label := $Label
+onready var timer := $Timer
+
+
+func _ready():
+	InputManager.connect("device_changed", self, "_on_Device_changed")
+	timer.connect("timeout", self, "_on_Timeout")
+	_on_Device_changed(InputManager.device, 0)
+	timer.start()
+
+
+func anim_pulse() -> void:
+	anim.play("pulse")
+
+
+func toggle_icon(icon: int) -> void:
+	if icon == Icon.ALT:
+		next_icon = Icon.DEFAULT
+		texture = alt_icon_texture
+		return
+	next_icon = Icon.ALT
+	texture = icon_texture
+
+
+func _on_Device_changed(device: String, device_index: int) -> void:
+	var joy_string := InputManager.get_device_button_from_action(action, device)
+	icon_texture = InputManager.get_device_icon_texture_from_action(joy_string, device)
+	alt_icon_texture = InputManager.get_device_icon_texture_from_action(joy_string, device, true)
+
+	if icon_texture == null:
+		icon_texture = InputManager.get_device_icon_texture_fallback(device)
+		label.text = joy_string
+		label.show()
+
+	if alt_icon_texture == null:
+		alt_icon_texture = InputManager.get_device_icon_texture_fallback(device)
+
+	texture = icon_texture
+	next_icon = Icon.ALT
+
+func _on_Timeout() -> void:
+	print(next_icon)
+	toggle_icon(next_icon)
