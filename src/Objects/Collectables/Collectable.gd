44ba75fb1a7b8b401709099af0ebc1bed0f699f2
levrault <lucf.langis@gmail.com>
2022-03-30 09:44:49 -0400

    Fix carrot stange vibrating bug

--- src/Objects/Collectables/Collectable.gd
+++ src/Objects/Collectables/Collectable.gd
@@ -10,20 +10,12 @@ onready var sfx := $SFXCollectable
 
 func _ready() -> void:
 	connect("body_entered", self, "_on_Player_entered")
-	Events.connect("player_character_changed", self, "_on_Player_character_changed")
 	feedback.anim.connect("animation_finished", self, "_on_Feedback_animation_finished")
-
-	_on_Player_character_changed()
-
-
-func _on_Player_character_changed() -> void:
-	if character != Character.selected:
-		collision.disabled = true
-		return
-	collision.disabled = false
 
 
 func _on_Player_entered(body: Player) -> void:
+	if character != Character.selected:
+		return
 	Character.update_score()
 	skin.hide()
 	collision.call_deferred("set", "disabled", true)
