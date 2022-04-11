46b23ed2dbedb2fb5b1f8d08d0c3d2da36d25766
levrault <lucf.langis@gmail.com>
2022-04-11 14:26:55 -0400

    Display a notification when device change

---
+++ src/UI/Commons/Notifications/DeviceChangedNotification.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+var current_device := InputManager.device
+onready var anim := $AnimationPlayer
+onready var label := $PanelContainer/MarginContainer/Label
+
+
+func _ready():
+	InputManager.connect("device_changed", self, "_on_Device_changed")
+	current_device = InputManager.device
+
+
+func _on_Device_changed(device: String, device_index: int) -> void:
+	if device == InputManager.DEVICE_MOUSE:
+		device = InputManager.DEVICE_KEYBOARD
+
+	if device == current_device:
+		return
+
+	label.text = tr("commons.device_changed").format({device = device})
+	anim.play("show")
+	current_device = device
