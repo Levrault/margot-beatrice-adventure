71c2c08571c2136c4ce2fc6e9d6650616a240b10
Luc-Frédéric Langis <lucf.langis@gmail.com>
2022-04-20 08:24:25 -0400

    Add new plant design (#46)

--- src/Enemies/Statics/States/Attack.gd
+++ src/Enemies/Statics/States/Attack.gd
@@ -5,10 +5,12 @@ func enter(msg: Dictionary = {}) -> void:
 	owner.skin.play("attack")
 	owner.skin.connect("animation_finished", self, "_on_Skin_animation_finished")
 	owner.skin.connect("damage_source_enabled", owner.damage_source, "set_active", [true])
+	owner.skin.connect("damage_source_disabled", owner.damage_source, "set_active", [false])
 
 
 func exit() -> void:
 	owner.skin.disconnect("damage_source_enabled", owner.damage_source, "set_active")
+	owner.skin.disconnect("damage_source_disabled", owner.damage_source, "set_active")
 	owner.skin.disconnect("animation_finished", self, "_on_Skin_animation_finished")
 
 
