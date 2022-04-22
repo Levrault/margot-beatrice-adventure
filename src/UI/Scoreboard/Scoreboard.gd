16ffb75a887fa78eb744e2792784edcd2ed232c8
levrault <lucf.langis@gmail.com>
2022-04-22 16:05:22 -0400

    Update level 4 stats, change rank time to be set in seconds

--- src/UI/Scoreboard/Scoreboard.gd
+++ src/UI/Scoreboard/Scoreboard.gd
@@ -1,9 +1,9 @@
 extends Control
 
 # miliseconds
-export var time_for_rank_100 := 30000.0
-export var time_for_rank_80 := 45000.0
-export var time_for_rank_60 := 60000.0
+export var time_for_rank_100 := 30.0
+export var time_for_rank_80 := 45.0
+export var time_for_rank_60 := 60.0
 
 var current_level := ""
 var next_level := ""
@@ -15,6 +15,10 @@ func _ready():
 	Events.connect("level_finished", self, "_on_Level_finished")
 	current_level = owner.get_name().to_lower()
 	next_level = owner.next_level
+	
+	time_for_rank_100 *= 1000
+	time_for_rank_80 *= 1000
+	time_for_rank_60 *= 1000
 
 
 func _on_Level_finished() -> void:
